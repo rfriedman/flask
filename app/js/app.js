7ebd80cc20346b199e8ad11996ce1d8a2a8a666c
rfriedman <friedman.richard@gmail.com>
2018-01-22 11:48:36 -0500

    folders

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+angular.module('app', [])
+.controller('Hello', function($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+});
---
+++ app/python/app.py
@@ -0,0 +1,37 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+
+def get_tasks():
+    record=dict()
+    task=list()
+
+    record['id'] = 1
+    record['title'] = 'Buy groceries'
+    task.append(record)
+    record = dict()
+    record['id'] = 2
+    record['title'] = 'Learn python'
+    task.append(record)
+
+    return jsonify({'tasks': task})
+
+if __name__ == '__main__':
+
+ app.run(debug=True)
+
+
+ def __init__(self, batchfile):
+      with open(batchfile,'r') as data:
+        self.batch = json.load(data)
+      self.hostuser = self.batch['hostuser']
+      self.clientuser = self.batch['clientuser']
+      self.hostlist = self.batch['hostlist']
+      self.joblist = self.batch['joblist']
+      self.node = dict()
+      self.proclist = list()
+      self.cmds = list()
+
