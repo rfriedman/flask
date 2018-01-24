8f9c2f8785d94241b418f7ba3c6d01300a8b7abf
rfriedman <friedman.richard@gmail.com>
2018-01-24 21:02:13 +0000

    login api

--- app/app.py
+++ app/app.py
@@ -1,24 +1,25 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, jsonify, request, redirect, render_template
 
 app = Flask(__name__)
 users = list()
 record = {"usrid":"1198","usrname":"rfriedman"}
+session = list()
 users.append(record)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-      direct = redirect('http://google.com')
       if request.form['usrid']:
        usrid = request.form['usrid']
        for u in users:
         if u['usrid'] == usrid:
-          direct = redirect('/index')
-      return direct
+           record={"sessionid":usrid,"usrid":usrid}
+           session.append(record)
+      return record
 
 @app.route('/index', methods=['GET'])
-def index():
-    return redirect('http://google.com') 
+def index(name=None):
+    return render_template('index.html') 
     
 
 if __name__ == '__main__':
